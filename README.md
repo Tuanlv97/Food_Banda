7d329dc392c55052b8b387d1fc9407924dcbf29c
Thợ Code <anhtuan20011997@gmail.com>
2023-09-24 16:15:47 +0700

    Initial Project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Food_Banda
+# h1
---
+++ lib/authentication/login.dart
@@ -0,0 +1,91 @@
+
+import 'package:flutter/material.dart';
+import 'package:food_banda/widgets/custom_text_field.dart';
+import 'package:food_banda/widgets/error_dialog.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+{
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  formValidation()
+  {
+    if(emailController.text.isNotEmpty && passwordController.text.isNotEmpty)
+    {
+      //login
+   //   loginNow();
+    }
+    else
+    {
+      showDialog(
+          context: context,
+          builder: (c)
+          {
+            return ErrorDialog(
+              message: "Please write email/password.",
+            );
+          }
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Image.asset("images/seller.png", height: 270,),
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  data: Icons.email,
+                  controller: emailController,
+                  hintText: "Email",
+                  isObsecre: false,
+                ),
+                CustomTextField(
+                  data: Icons.lock,
+                  controller: passwordController,
+                  hintText: "Password",
+                  isObsecre: true,
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            child: const Text(
+              "Login",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.cyan,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            ),
+            onPressed: ()
+            {
+              formValidation();
+            },
+          ),
+          const SizedBox(height: 30,),
+        ],
+      ),
+    );
+  }
+}
